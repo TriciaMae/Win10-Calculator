5cbd00ce6f9600b691119d41eb96dece46249022
Tet <58018345+TriciaMae@users.noreply.github.com>
2020-01-11 19:23:30 +0800

    Add files via upload

---
+++ SciCal/SciCal/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Private Sub SplitContainer1_SplitterMoved(sender As Object, e As SplitterEventArgs)
+
+    End Sub
+
+    Private Sub XToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles XToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub Button34_Click(sender As Object, e As EventArgs) Handles Button34.Click
+        Label14.Text = "0"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Label14.Text = Label14.Text + "0"
+    End Sub
+
+    Private Sub Button33_Click(sender As Object, e As EventArgs) Handles Button33.Click
+        Label14.Text = Label14.Text + "1"
+    End Sub
+
+    Private Sub Button37_Click(sender As Object, e As EventArgs) Handles Button37.Click
+        Label14.Text = Label14.Text + "2"
+    End Sub
+
+    Private Sub Button41_Click(sender As Object, e As EventArgs) Handles Button41.Click
+        Label14.Text = Label14.Text + "3"
+    End Sub
+
+    Private Sub Button32_Click(sender As Object, e As EventArgs) Handles Button32.Click
+        Label14.Text = Label14.Text + "4"
+    End Sub
+
+    Private Sub Button36_Click(sender As Object, e As EventArgs) Handles Button36.Click
+        Label14.Text = Label14.Text + "5"
+    End Sub
+
+    Private Sub Button40_Click(sender As Object, e As EventArgs) Handles Button40.Click
+        Label14.Text = Label14.Text + "6"
+    End Sub
+
+    Private Sub Button31_Click(sender As Object, e As EventArgs) Handles Button31.Click
+        Label14.Text = Label14.Text + "7"
+    End Sub
+
+    Private Sub Button35_Click(sender As Object, e As EventArgs) Handles Button35.Click
+        Label14.Text = Label14.Text + "8"
+    End Sub
+
+    Private Sub Button39_Click(sender As Object, e As EventArgs) Handles Button39.Click
+        Label14.Text = Label14.Text + "9"
+    End Sub
+End Class
